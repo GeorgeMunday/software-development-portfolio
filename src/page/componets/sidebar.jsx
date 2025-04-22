89d1ec8136fe8645f64255ff13a9185d0f9af455
John Doe <[email protected]>
2025-04-22 22:48:08 +0100

    co mmit

--- src/page/componets/sidebar.jsx
+++ src/page/componets/sidebar.jsx
@@ -1,32 +1,45 @@
 import React from 'react';
 
+import "./sidebar.css";
 
 const Sidebar = () => {
     return (
         <aside className='sidebar'>
             <div className="sidebar__title">
-                <img src="https://via.placeholder.com/150" alt="Profile" className="sidebar__title--img" />
+                <img src="self.webp" alt="Profile" className="sidebar__title--img" />
                 <h1 className="sidebar__title--text">George Munday</h1>
                 <p className="sidebar__title--subtitle">Web Developer</p>
             </div>
-            <main>
+            <main className='sidebar__main'>
                 <ul className="sidebar__list">
-                    <div>
-                        <img src="https://via.placeholder.com/20" alt="phone" className="sidebar__list--icon" />
-                        <li className="sidebar__list--item">email</li>
-                    </div>
-                    <div>
-                        <img src="https://via.placeholder.com/20" alt="phone" className="sidebar__list--icon" />
-                        <li className="sidebar__list--item"><a href="#">LinkedIn</a></li>
-                    </div>
-                    <div>
-                        <img src="https://via.placeholder.com/20" alt="phone" className="sidebar__list--icon" />    
-                        <li className="sidebar__list--item"><a href="#">Github</a></li>
-                    </div>
-                    <div>
-                        <img src="https://via.placeholder.com/20" alt="phone" className="sidebar__list--icon" />
-                        <li className="sidebar__list--item">phone number</li>
-                    </div>
+                    <li className="sidebar__list--item-container">
+                        <img src="email.png" alt="email" className="sidebar__list--icon" />
+                        <div className="sidebar__list--item--nested">
+                            <span className="sidebar__list--header">email</span>
+                            <span className="sidebar__list--info">george.munday@dedalus.com</span>
+                        </div>
+                    </li>
+                    <li className="sidebar__list--item-container">
+                        <img src="linkedin.png" alt="LinkedIn" className="sidebar__list--icon" />
+                        <div className="sidebar__list--item--nested">
+                            <span className="sidebar__list--header">LinkedIn</span>
+                            <a href="#" className="sidebar__list--info">linkedin.com/in/georgemunday</a>
+                        </div>
+                    </li>
+                    <li className="sidebar__list--item-container">
+                        <img src="github-logo.png" alt="Github" className="sidebar__list--icon" />
+                        <div className="sidebar__list--item--nested">
+                            <span className="sidebar__list--header">Github</span>
+                            <a href="#" className="sidebar__list--info">github.com/georgemunday</a>
+                        </div>
+                    </li>
+                    <li className="sidebar__list--item-container">
+                        <img src="phone-call.png" alt="Phone" className="sidebar__list--icon" />
+                        <div className="sidebar__list--item--nested">
+                            <span className="sidebar__list--header">Phone</span>
+                            <span className="sidebar__list--info">07908654321</span>
+                        </div>
+                    </li>
                 </ul>
             </main>
         </aside>
