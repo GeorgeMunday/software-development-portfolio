3031aaf2da5ac0af71480ab34c9bb8e674adeb2a
John Doe <[email protected]>
2025-04-23 10:51:01 +0100

    header done

--- src/page/componets/header.jsx
+++ src/page/componets/header.jsx
@@ -1,4 +1,5 @@
 import react from 'react'
+import { useState } from 'react'
 
 import "./header.css" 
 
@@ -6,8 +7,25 @@ export default function Header() {
     return (
         <header className='header'>
                 <div className="header__title">
-                    <p className="header__title--text">George Munday</p>
+                    <h1 className='header__title--text'>About</h1>
+                    <div className= 'underline'></div>
                 </div>
+                <nav className='header__nav'>
+                    <ul className='header__nav--list'>
+                        <li className='header__nav--item'>
+                            <button className='header__nav--button1' >About</button>
+                        </li>
+                        <li className='header__nav--item'>
+                            <button className='header__nav--button2'>Resume</button>
+                        </li>
+                        <li className='header__nav--item'>
+                            <button className='header__nav--button3'>Portfolio</button>
+                        </li>
+                        <li className='header__nav--item'>
+                            <button className='header__nav--button4' id= '2'>Contact</button>
+                        </li>
+                    </ul>
+                </nav>
         </header>
     )
     }
