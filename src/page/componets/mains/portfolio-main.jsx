65f61b614b05213d8f41cea1decf8d1b8aff7dc2
John Doe <[email protected]>
2025-05-05 22:12:59 +0100

    vvv

--- src/page/componets/mains/portfolio-main.jsx
+++ src/page/componets/mains/portfolio-main.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Carousel from "./portfolio-components/carousel.jsx";
 import Intro from "./about-components/intro.jsx";
 import TableComponent from "./portfolio-components/table.jsx";
+import Cards from "./portfolio-components/back-end.jsx";
 
+import './portfolio.css'
 
 const Projects = [
     {name: 1, Demo: "#", Code: "#","End Date": 13},
@@ -15,13 +17,38 @@ const Projects = [
     {name: 1, Demo: "#", Code: "#","End Date": 13},
     {name: 2, Demo: "#", Code: "#","End Date": 13},
     {name: 3, Demo: "#", Code: "#","End Date": 13}
-]
+];
 
 const Portfolio = () => {
+    const [activeComponent, setActiveComponent] = useState('table');
+
+    const renderComponent = () => {
+        switch (activeComponent) {
+            case 'carousel':
+                return <Intro/>;
+            case 'intro':
+                return <Cards/>;
+            case 'table':
+            default:
+                return <TableComponent data = {Projects} />;
+        };
+    };
+
     return (
         <>
-            <div className="portfolio-main">
-                <TableComponent data={Projects}></TableComponent>
+            <nav className="portfolio__nav">
+                <button className="portfolio__button" onClick={() => setActiveComponent('table')}>
+                    Whole portfolio
+                </button>
+                <button className="portfolio__button" onClick={() => setActiveComponent('carousel')}>
+                    Web development
+                </button>
+                <button className="portfolio__button" onClick={() => setActiveComponent('intro')}>
+                    Back end design
+                </button>
+            </nav>
+            <div className="portfolio__content">
+                {renderComponent()}
             </div>
         </>
     );
